54aa8dbaeba1989bdaaad745c1e5109e09b15019
IDPMJH <jong000829@gmail.com>
2025-03-18 16:21:31 +0900

    Change_at_home

--- STL_base/STL/main_week_3.cpp
+++ STL_base/STL/main_week_3.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 
 
-#define Prac 7
+#define Prac 4
 
 #if Prac == 1
 #include <iostream>
@@ -22,7 +22,13 @@ int main()
 	// int 1000개를 저장하기 위해 얼마만큼의 파일 바이트를 사용하였는가? 
 	// => 8200byte
 	// ========================================================
-	// ★★★ 32bit int의 최대 값인 21억 어쩌구까지 파일에 저장하면, 평균 byte 수는?(text mode)
+	// ★★★ 32bit int의 최대 값인 21억 어쩌구까지 파일에 textmode로 저장하면, 평균 byte 수는?(text mode)
+	// -> 평균적으로 10자리 수가 나올 확률이 가장 높음 -> 10자리로 입력한다고 가정 = 10byte
+	// unsigned가 아니므로 부호가 50% 확률로 붙음 -> 각 수에 0.5바이트 추가 = 10.5byte
+	// 각 수의 개행 문자 포함 = 11.5byte
+	// 수의 개수 = 1000, 총 byte 수 = 1000 * 11.5 = 11500byte (예상)
+	// 혹은 절반 정도의 나올 확률을 가진 9자리 수를 절반 개수만큼 계산시, 
+	// 10.5byte* 500 + 11.5byte*500 = 11000byte (예상)
 
 
 
@@ -79,9 +85,12 @@ int main()
 	//ofstream out{ "int 1000개 메모리 그대로" }; -> mode 설정이 없으면 text mode
 	// text mode로 설정 시, 운영체제가 보기 쉽게 해주는 작업을 함
 	// 
-	ofstream out{ "int 1000개 메모리 그대로", ios::binary };
+	ofstream out{ "int 1000개 메모리 그대로" ,ios::binary};
+	// int a = arr.size();  a == 1000 -> 원소의 개수
+	// a = sizeof(arr);		a == 4000 -> 배열 자체의 크기
 
-	out.write(reinterpret_cast<char*>(arr.data()), sizeof(int)*arr.size());
+	//out.write(reinterpret_cast<char*>(arr.data()), sizeof(int)*arr.size());
+	out.write(reinterpret_cast<char*>(arr.data()),sizeof(arr));
 }
 
 
@@ -94,8 +103,11 @@ int main()
 int main()
 {
 	// [상황] 현재 하나의 int 4바이트는 0A 0A 0A 0A로 이루어져 있다.
-	// -> 텍스트 파일로 Windows에서 -> 0A(라인피드),0D(캐리지 리턴)으로 변환
-
+	// -> 텍스트 파일로 Windows에서 -> 0D(캐리지 리턴),0A(라인피드)으로 변환
+	// Windows의 텍스트 모드에서 0A → 0D 0A 변환이 발생했기 때문에 
+	// 파일 크기가 4000바이트 → 8000바이트로 증가한 것.
+	// 이를 방지하려면 바이너리 모드(std::ios::binary)를 사용하여 변환을 막아야 함.
+	// 참고로 EOF(End Of File)는 0x1A로 표현되며, 파일의 크기와는 무관하게 파일의 끝을 나타냄.
 	array<int, 1000> arr{};
 	arr.fill(0x0a0a0a0a);
 
@@ -156,8 +168,7 @@ int main()
 	// 읽을 때 text mode로 열면?
 	//ifstream in{ "int 10만개를 바이너리모드 write함수로 기록" };
 	if (not in)
-		exit(1);
-
+		return;
 	// 스택 공간 매우 많이 차지	
 	array<int, 10'0000> arr{};
 
